4eec582aa4573c0f66da9f26a835d63073e2b880
andy922200-m1 <andy922200@gmail.com>
2023-05-27 10:23:12 +0800

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html", "./src/**/*.css", "./js/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
