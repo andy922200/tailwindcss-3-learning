2fc62c6940bcdf969bc5cc79df9ca096cdda8337
andy922200-m1-company <andy922200@gmail.com>
2023-06-14 13:34:15 +0800

    add custom base settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
+const plugin = require('tailwindcss/plugin')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*.html", "./src/**/*.css", "./js/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ addBase, theme }){
+        addBase({
+            h1: { fontSize: theme('fontSize.2xl') }
+        })
+    })
+  ],
 }
 
